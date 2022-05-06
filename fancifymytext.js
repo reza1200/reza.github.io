eaba1f0e71438602b7fb2be6ebd9d9c9305ff6a8
reza1200 <rsalmanian@csus.edu>
2022-05-06 14:53:29 -0700

    first commit

---
+++ fancifymytext.js
@@ -0,0 +1,14 @@
+
+function myfunction(){
+    var str = document.getElementById('text').style.fontSize ='1em';
+    var parts = str.split(",").join("_");
+    //document.getElementById('text').style = 'text-decoration: underline ';
+      
+}
+function x(){
+    alert("Hello,World!");
+}
+function y(){
+    document.getElementById('text').add.suffix = "_Moo";
+
+}
